5340cea4a4588c6724108678116c79fea1cd304a
유재건 <neversaydie374@gmail.com>
2023-10-23 21:15:36 +0900

    19. NotFound

--- main.js
+++ main.js
@@ -7,46 +7,41 @@ var url = require('url'); //require 요구하다
 var app = http.createServer(function (request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
+  var pathName = url.parse(_url, true).pathname;
   var title = queryData.id;
-  console.log("quertDATA OBJ.id: ", queryData.id);
-  // console.log(_url);
-  if (_url == '/') {
-    // _url = '/index.html'; // '/' 이면 index로 보내준다.
-    title = 'Welcome';
+  const ERROR_MESG = "NOT FOUND";
+  // console.log("url parse:"+ url.parse(_url, true));
+  // console.log("url parse.pathName:"+ url.parse(_url, true).pathname);
+  if (pathName === '/') {
+    fs.readFile(`data/${title}`, 'UTF-8', (err, description) => {
+      var template = `
+    <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    <ol>
+      <li><a href="/?id=HTML">HTML</a></li>
+      <li><a href="/?id=CSS">CSS</a></li>
+      <li><a href="/?id=JavaScript">JavaScript</a></li>
+    </ol>
+    <h2>${title}</h2>
+   <p>
+   ${description}
+   </p>
+  </body>
+  </html>
+    `;
+      response.writeHead(200);
+      response.end(template);
+    });
+  } else {
+    response.writeHead(404);
+    response.end(ERROR_MESG);
   }
-  if (_url == '/favicon.ico') {
-    return response.writeHead(404); //실패 코드
-  }
-  response.writeHead(200); //성공코드
-  // console.log(__dirname + url)
-  // response.end(fs.readFileSync(__dirname + _url));
-  fs.readFile(`data/${title}`, 'UTF-8', (err, description) => {
-    var template = `
-  <!doctype html>
-<html>
-<head>
-  <title>WEB1 - ${title}</title>
-  <meta charset="utf-8">
-</head>
-<body>
-  <h1><a href="/">WEB</a></h1>
-  <ol>
-    <li><a href="/?id=HTML">HTML</a></li>
-    <li><a href="/?id=CSS">CSS</a></li>
-    <li><a href="/?id=JavaScript">JavaScript</a></li>
-  </ol>
-  <h2>${title}</h2>
- <p>
- ${description}
- </p>
-</body>
-</html>
-  `;
-    response.end(template); // == 화면 출력해주는 곳 같다.
-  });
-
-
-  // response.end('egoing : ' + url);
 
 });
 app.listen(3000);
