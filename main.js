2e9af7fdfc6446ac686236b6bfff0b793d334acf
유재건 <neversaydie374@gmail.com>
2023-10-25 23:03:30 +0900

    first_half end

--- main.js
+++ main.js
@@ -3,6 +3,37 @@ var fs = require('fs');
 var url = require('url'); //require 요구하다
 const ERROR_MESG = "NOT FOUND";
 //모듈(그룹핑)
+function templateHTML(title, list, body) {
+  return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    ${body}  
+  </body>
+  </html>`
+
+}
+
+function templateList(filelist){
+  let list = '<ul>';
+  let i = 0;
+  while (i < filelist.length) {
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+    i = i + 1;
+  }
+  list = list + '</ul>';
+  return list;
+}
+function resSuccess(template) {
+  response.writeHead(200);
+  response.end(template);
+}
 
 
 var app = http.createServer(function (request, response) {
@@ -11,84 +42,29 @@ var app = http.createServer(function (request, response) {
   var pathName = url.parse(_url, true).pathname;
   if (pathName === '/') {
     if (queryData.id === undefined) {
-
       fs.readdir('./data', (err, filelist) => {
-        console.log("fs는 무엇인가? : " + fs);
-        console.log(filelist);
         let title = 'Welcome';
         let description = "Hello node.js";
-        let list = '<ul>';
-        let i = 0;
-        while (i < filelist.length) {
-          list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-          i = i + 1;
-        }
-        list = list + '</ul>';
-        //map으로 변경 가능할 것 같다.
-        var template = `
-        <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        ${list}
-        <h2>${title}</h2>
-       <p>
-       ${description}
-       </p>
-      </body>
-      </html>
-        `;
-        response.writeHead(200);
-        response.end(template);
+        let body = `<h2>${title}</h2>${description}`
+        let list = templateList(filelist);
+        var template = templateHTML(title, list, body);
+        resSuccess(template);
       });
-
-
     } else {
       fs.readdir('./data', (err, filelist) => {
-        console.log("fs는 무엇인가? : " + fs);
-        console.log(filelist);
-        let title = 'Welcome';
-        let description = "Hello node.js";
-        let list = '<ul>';
-        let i = 0;
-        while (i < filelist.length) {
-          list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-          i = i + 1;
-        }
-        list = list + '</ul>';
-
-      fs.readFile(`data/${queryData.id}`, 'UTF-8', (err, description) => {
-        let title = queryData.id;
-        var template = `
-        <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        ${list}
-        <h2>${title}</h2>
-       <p>
-       ${description}
-       </p>
-      </body>
-      </html>
-        `;
-        response.writeHead(200);
-        response.end(template);
+        fs.readFile(`data/${queryData.id}`, 'UTF-8', (err, description) => {
+          let title = queryData.id;
+          let body = `<h2>${title}</h2>${description}`
+          let list = templateList(filelist);
+          var template = templateHTML(title, list, body);
+          resSuccess(template);
+        });
       });
-    });
     }
   } else {
     response.writeHead(404);
     response.end(ERROR_MESG);
   }
 });
-app.listen(3000);
+app.listen(3001);
 //포트번호
